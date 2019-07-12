1cb1049498b228c4fcdd83c01960404fbdc52071
shanejonas <jonas.shane@gmail.com>
2019-07-11 19:38:15 -0700

    fix: add better sed

--- .pristine/post-install.sh
+++ .pristine/post-install.sh
@@ -5,6 +5,8 @@ RED="\033[0;31m"
 NC="\033[0m" # No Color
 defaultWebsiteName="pristine.builders"
 defaultName="Pristine"
+defaultLightLogo="https://raw.githubusercontent.com/etclabscore/jade-media-assets/master/jade-logo-light/jade-logo-light%20(PNG)/256x256.png"
+defaultDarkLogo="https://raw.githubusercontent.com/etclabscore/jade-media-assets/master/jade-logo-dark/jade-logo-dark%20(PNG)/256x256.png"
 
 echo ""
 echo "💎  Welcome Pristine Website Post-Install setup! 💎"
@@ -21,9 +23,27 @@ read websiteName
 
 echo ""
 
+echo -e "${BLUE}Enter the light logo URL${NC}"
+
+read lightLogo
+
+echo ""
+
+echo -e "${BLUE}Enter the dark logo URL${NC}"
+
+read darkLogo
+
+echo ""
+
 # using ~ in place of / to avoid slashes in package names conflicting with sed
-sed -i  "" -e "s~${defaultWebsiteName}~${websiteName}~g"  docs/**/*
-sed -i  "" -e "s~${defaultName}~${name}~g"  docs/**/*
+find ./docs/**/* -type f -exec sed -i "" -e "s~${defaultWebsiteName}~${websiteName}~g" {} \;
+find ./docs/**/* -type f -exec sed -i "" -e "s~${defaultName}~${name}~g" {} \;
+
+sed -i  "" -e "s~${defaultWebsiteName}~${websiteName}~g" README.md
+sed -i  "" -e "s~${defaultName}~${name}~g" README.md
+
+find ./docs/**/* -type f -exec sed -i "" -e "s~${defaultDarkLogo}~${darkLogo}~g" {} \;
+find ./docs/**/* -type f -exec sed -i "" -e "s~${defaultLightLogo}~${lightLogo}~g" {} \;
 
 echo -e "${BLUE} 🚀  Project Setup Completed. 🚀"
 
